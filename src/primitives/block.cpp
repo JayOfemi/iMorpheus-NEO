a2ff44163386518ea565c28f67fbf432d82e8e9c
taichenliu <taichen.liu@imorpheus.ai>
2018-06-07 11:05:36 +0800

    modify primitives, bitcoin codes are added

---
+++ src/primitives/block.cpp
@@ -0,0 +1,43 @@
+/*
+ Copyright (c) 2009-2010 Satoshi Nakamoto
+ Copyright (c) 2009-2014 The Bitcoin Core Developers
+ Copyright (c) 2018- The iMorpheus Core Developers
+ Distributed under the MIT/X11 software license, see the accompanying
+ file COPYING or http://www.opensource.org/licenses/mit-license.php.
+*/
+
+#include <primitives/block.h>
+
+#include <hash.h>
+#include <tinyformat.h>
+#include <utilstrencodings.h>
+#include <crypto/common.h>
+
+void CBlockHeader::SetAuxpow (CAuxPow* apow)
+{
+    if (apow)
+    {
+        auxpow.reset(apow);
+        SetAuxpowVersion(true);
+    } else
+    {
+        auxpow.reset();
+        SetAuxpowVersion(false);
+    }
+}
+
+std::string CBlock::ToString() const
+{
+    std::stringstream s;
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+        GetHash().ToString(),
+        nVersion,
+        hashPrevBlock.ToString(),
+        hashMerkleRoot.ToString(),
+        nTime, nBits, nNonce,
+        vtx.size());
+    for (const auto& tx : vtx) {
+        s << "  " << tx->ToString() << "\n";
+    }
+    return s.str();
+}
